ef78dc7a3dfa03a62844cbad60d06b8a90739343
natalia <nataliastawowy@gmail.com>
2016-05-15 17:52:25 +0200

    commit

--- src/main/resources/static/app/cmp.js
+++ src/main/resources/static/app/cmp.js
@@ -1,5 +1,9 @@
 angular.module('antiplag', [])
 .controller('cmpController', function($http, $scope, $window) {
-
+  $http.get("/api/js/pastedContent")
+      .then(function(response) {
+          $scope.pastedCode=(response.data.code);
+          hljs.initHighlightingOnLoad();
+      });
 
 });
