1d12e19ad8c583b943f2b2de4299e6e4bb2c8f75
natalia <nataliastawowy@gmail.com>
2016-05-15 18:02:18 +0200

    highlighting

--- src/main/resources/static/app/cmp.js
+++ src/main/resources/static/app/cmp.js
@@ -2,8 +2,12 @@ angular.module('antiplag', [])
 .controller('cmpController', function($http, $scope, $window) {
   $http.get("/api/js/pastedContent")
       .then(function(response) {
-          $scope.pastedCode=(response.data.code);
-          hljs.initHighlightingOnLoad();
+            $scope.pastedCode=(response.data.code);
+            setTimeout(function() {
+              $('pre code').each(function(i, block) {
+                 hljs.highlightBlock(block);
+               });
+            }, 500);
       });
 
 });
